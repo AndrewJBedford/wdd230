780405b5ed608b80dcb8b24928ca0885a29bbe68
AndrewJBedford <andrew2018@byui.edu>
2021-09-18 14:58:30 -0400

    .

--- js/dates.js
+++ js/dates.js
@@ -1,5 +1,2 @@
-const dateTimeObject = new Date();
-const fullYear = dateTimeObject.getFullYear();
-const currentYear = document.getElementById("currentYear");
-currentYear.textContent = fullYear;
-document.getElementById("lastUpdated").textContent = document.lastmodified;
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textCotent = Date().getFullYear();
