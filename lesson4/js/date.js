d32ae070f2281ee0d6b01d08bb160553c2cedba6
AndrewJBedford <andrew2018@byui.edu>
2021-10-08 14:11:01 -0400

    .

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,8 +1,7 @@
-var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
-let modified = new Date(document.lastModified);
-let formattedDate = `Last updated: ${months[modified.getMonth()]} ${modified.getDate()}, ${modified.getUTCFullYear()}`;
-document.getElementById("modified").innerHTML = formattedDate;
-
-let today = new Date();
-let theYear = today.getFullYear();
-document.getElementById("year").innerHTML = theYear;
+let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+let d =new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fullDate = dayName + ", " + d.getDate() + " " + monthName + " "  + d.getFullYear();
+document.getElementById("currentDate").textContent = fullDate;
