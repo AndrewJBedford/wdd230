03b47abc9abe0c6885f38266b9f405c03ab3f568
AndrewJBedford <andrew2018@byui.edu>
2021-10-11 17:58:53 -0400

    .

---
+++ lesson5/js/date.js
@@ -0,0 +1,7 @@
+let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+let d =new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fullDate = dayName + ", " + d.getDate() + " " + monthName + " "  + d.getFullYear();
+document.getElementById("currentDate").textContent = fullDate;
