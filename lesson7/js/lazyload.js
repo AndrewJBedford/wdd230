4e51cb054d9852d12feb789d0cb2e06d77327b93
AndrewJBedford <andrew2018@byui.edu>
2021-10-28 21:36:27 -0400

    .

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,28 @@
+const imagesLoading = document.querySelectorAll("img[data-src]");
+
+const imageOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const preloadImage = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const imageObserver = new IntersectionObserver((items, observer) => {
+        items.forEach(item => {if(!item.isIntersecting) {
+        }});
+    }, imageOptions);
+
+    imagesLoading.forEach(img => {
+        imageObserver.observe(img);
+    });
+}
+else {
+  imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+}
