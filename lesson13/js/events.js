2fd43ba23b14ebdb527a264a6e0a5efc21eaa31e
AndrewJBedford <andrew2018@byui.edu>
2021-12-16 01:46:00 -0500

    ..

--- lesson13/js/events.js
+++ lesson13/js/events.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://raw.githubusercontent.com/AndrewJBedford/wdd230/master/directory/ocalaevents.json';
+const requestURL = 'https://github.com/AndrewJBedford/wdd230/blob/master/lesson13/directory/ocalaevents.json';
 
 fetch(requestURL)
     .then(function (response) {
